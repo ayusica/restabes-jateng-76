b22a4418d1c8c318418c4bd056ba0cc8a3a418bd
ayusica <72453272+ayusica@users.noreply.github.com>
2020-10-23 09:49:20 +0700

    Update Panduan.php

--- application/controllers/Panduan.php
+++ application/controllers/Panduan.php
@@ -9,14 +9,13 @@ class Panduan extends CI_Controller
     {
         if ($this->session->userdata('masuk') == FALSE) {
             redirect('login');
-        } else {
-            $data['judul'] = "Panduan Penggunaan Sistem";
-
-            $this->load->view('templates/user/header_user', $data);
-            $this->load->view('templates/user/sidebar');
-            $this->load->view('templates/user/topbar');
-            $this->load->view('panduan/index');
-            $this->load->view('templates/user/footer');
         }
+        $data['judul'] = "Panduan Penggunaan Sistem";
+
+        $this->load->view('templates/user/header_user', $data);
+        $this->load->view('templates/user/sidebar');
+        $this->load->view('templates/user/topbar');
+        $this->load->view('panduan/index');
+        $this->load->view('templates/user/footer');
     }
 }
