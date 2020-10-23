cc8d969ea00fbaa736c0b5c70140901a4a18f36f
ayusica <72453272+ayusica@users.noreply.github.com>
2020-10-23 09:33:30 +0700

    Update form_polsek.php

--- application/views/page/form_polsek.php
+++ application/views/page/form_polsek.php
@@ -4,7 +4,7 @@
   <title>Form Impor Data Personel Polsek</title>
 
   <!-- Load File jquery.min.js yang ada difolder js -->
-  <script src="<?php echo base_url('assets/js/jquery-3.4.1.min.js'); ?>"></script>
+  <script src="<?= filter_var(base_url('assets/js/jquery-3.4.1.min.js'), FILTER_DEFAULT); ?>"></script>
 
   <script>
     $(document).ready(function() {
@@ -18,13 +18,13 @@
   <h3>Form Impor Data Personel</h3>
   <hr>
 
-  <a href="<?php echo base_url("assets/excel/format.xlsx"); ?>">Unduh Format</a>
+  <a href="<?= filter_var(base_url("assets/excel/format.xlsx"), FILTER_DEFAULT); ?>">Unduh Format</a>
   <br>
   <br>
 
   <!-- Buat sebuah tag form dan arahkan action nya ke controller ini lagi -->
 
-  <form method="post" action="<?= base_url("sium/form_polsek") . '?id=' . $this->uri->segment(3) ?>" enctype="multipart/form-data">
+  <form method="post" action="<?= filter_var(base_url("sium/formPolsek") . '?id=' . $this->uri->segment(3), FILTER_DEFAULT) ?>" enctype="multipart/form-data">
     <!-- 
     -- Buat sebuah input type file
     -- class pull-left berfungsi agar file input berada di sebelah kiri
@@ -38,129 +38,134 @@
   </form>
 
   <?php
-  if (isset($_POST['preview'])) { // Jika user menekan tombol Preview pada form 
+  if (FILTER_INPUT(INPUT_POST, 'preview')) { // Jika user menekan tombol Preview pada form 
     if (isset($upload_error)) { // Jika proses upload gagal
-      echo "<div style='color: red;'>" . $upload_error . "</div>"; // Muncul pesan error upload
-      die; // stop skrip
-    }
-
-    // Buat sebuah tag form untuk proses import data ke database
-    echo "<form method='post' action='" . base_url("sium/import_polsek/") . $instansi . "'>";
-
-    // Buat sebuah div untuk alert validasi kosong
-    echo "<div style='color: red;' id='kosong'>
-    Semua data belum diisi, Ada <span id='jumlah_kosong'></span> data yang belum diisi.
-    </div>";
-
-    echo "<table border='1' cellpadding='8'>
-    <tr>
-      <th colspan='11'>Tampilan Data</th>
-    </tr>
-    <tr>
-        <th>Nama</th>
-        <th>NRP</th>
-        <th>Pkt</th>
-        <th>Jabatan</th>
-        <th>Tempat</th>
-        <th>Tanggal Lahir</th>
-        <th>Agama</th>
-        <th>Suku</th>
-        <th>Tmt.jab</th>
-        <th>Bagian</th>
-        <th>id_instansi</th>
-    </tr>";
-
-    $numrow = 1;
-    $kosong = 0;
-
-    // Lakukan perulangan dari data yang ada di excel
-    // $sheet adalah variabel yang dikirim dari controller
-    foreach ($sheet as $row) {
-      // Ambil data pada excel sesuai Kolom
-      $nama = $row['A']; // Ambil data Nama
-      $nrp = $row['B']; // Ambil data nrp
-      $pkt = $row['C']; // Ambil data pangkat
-      $jabatan = $row['D']; // Ambil data jabatan
-      $tempat = $row['E']; // Ambil data tempat
-      $tgl_lahir = $row['F']; // Ambil data tanggal lahir
-      $agama = $row['G']; // Ambil data agama
-      $suku = $row['H']; // Ambil data suku
-      $tmt_jab = $row['I']; // Ambil data tmt jab
-      $id_bagian = $row['J']; // Ambil data bagian
-      $id_instansi = $this->uri->segment(3); // Ambil data id instansi
-
-      // Cek jika semua data tidak diisi
-      if ($nama == "" && $nrp == "" && $pkt == "" && $jabatan == "" && $tempat == "" && $tgl_lahir == "" && $agama == "" && $suku == "" && $tmt_jab == "" && $id_bagian == "")
-        continue; // Lewat data pada baris ini (masuk ke looping selanjutnya / baris selanjutnya)
-
-      // Cek $numrow apakah lebih dari 1
-      // Artinya karena baris pertama adalah nama-nama kolom
-      // Jadi dilewat saja, tidak usah diimport
-      if ($numrow > 1) {
-        // Validasi apakah semua data telah diisi
-        $nama_td = (!empty($nama)) ? "" : " style='background: #E07171;'";
-        $nrp_td = (!empty($nrp)) ? "" : " style='background: #E07171;'";
-        $pkt_td = (!empty($pkt)) ? "" : " style='background: #E07171;'";
-        $jabatan_td = (!empty($jabatan)) ? "" : " style='background: #E07171;'";
-        $tempat_td = (!empty($tempat)) ? "" : " style='background: #E07171;'";
-        $tgl_lahir_td = (!empty($tgl_lahir)) ? "" : " style='background: #E07171;'";
-        $agama_td = (!empty($agama)) ? "" : " style='background: #E07171;'";
-        $suku_td = (!empty($suku)) ? "" : " style='background: #E07171;'";
-        $tmt_jab_td = (!empty($tmt_jab)) ? "" : " style='background: #E07171;'";
-        $id_bagian_td = (!empty($id_bagian)) ? "" : " style='background: #E07171;'";
-        $id_instansi_td = $this->uri->segment(3);
-
-        // Jika salah satu data ada yang kosong
-        if ($nama == "" or $nrp == "" or $pkt == "" or $jabatan == "" or $tempat == "" or $tgl_lahir == "" or $agama == "" or $suku == "" or $tmt_jab == "" or $id_bagian == "") {
-          $kosong++; // Tambah 1 variabel $kosong
-        }
-
-        echo "<tr>";
-        echo "<td" . $nama_td . ">" . $nama . "</td>";
-        echo "<td" . $nrp_td . ">" . $nrp . "</td>";
-        echo "<td" . $pkt_td . ">" . $pkt . "</td>";
-        echo "<td" . $jabatan_td . ">" . $jabatan . "</td>";
-        echo "<td" . $tempat_td . ">" . $tempat . "</td>";
-        echo "<td" . $tgl_lahir_td . ">" . $tgl_lahir . "</td>";
-        echo "<td" . $agama_td . ">" . $agama . "</td>";
-        echo "<td" . $suku_td . ">" . $suku . "</td>";
-        echo "<td" . $tmt_jab_td . ">" . $tmt_jab . "</td>";
-        echo "<td" . $id_bagian_td . ">" . $id_bagian . "</td>";
-        echo "<td" . $id_instansi_td . ">" . $instansi . "</td>";
-        echo "</tr>";
-      }
-
-      $numrow++; // Tambah 1 setiap kali looping
-    }
-
-    echo "</table>";
-
-    // Cek apakah variabel kosong lebih dari 0
-    // Jika lebih dari 0, berarti ada data yang masih kosong
-    if ($kosong > 0) {
   ?>
-      <script>
-        $(document).ready(function() {
-          // Ubah isi dari tag span dengan id jumlah_kosong dengan isi dari variabel kosong
-          $("#jumlah_kosong").html('<?php echo $kosong; ?>');
+      <!--Muncul pesan error upload-->
+      <div style='color: red;'><?= filter_var($upload_error, FILTER_DEFAULT) ?></div>
+    <?php
+      die; // stop skrip
+    } ?>
 
-          $("#kosong").show(); // Munculkan alert validasi kosong
-        });
-      </script>
+    <!-- Buat sebuah tag form untuk proses import data ke database -->
+    <form method='post' action=' <?= filter_var(base_url("sium/importPolsek/") . $instansi, FILTER_DEFAULT) ?>'>
+
+      <!-- Buat sebuah div untuk alert validasi kosong -->
+      <div style='color: red;' id='kosong'>
+        Semua data belum diisi, Ada <span id='jumlah_kosong'></span> data yang belum diisi.
+      </div>
+
+      <table border='1' cellpadding='8'>
+        <tr>
+          <th colspan='11'>Tampilan Data</th>
+        </tr>
+        <tr>
+          <th>Nama</th>
+          <th>NRP</th>
+          <th>Pkt</th>
+          <th>Jabatan</th>
+          <th>Tempat</th>
+          <th>Tanggal Lahir</th>
+          <th>Agama</th>
+          <th>Suku</th>
+          <th>Tmt.jab</th>
+          <th>Bagian</th>
+          <th>id_instansi</th>
+        </tr>
+        <?php
+        $numrow = 1;
+        $kosong = 0;
+
+        // Lakukan perulangan dari data yang ada di excel
+        // $sheet adalah variabel yang dikirim dari controller
+        foreach ($sheet as $row) {
+          // Ambil data pada excel sesuai Kolom
+          $nama = $row['A']; // Ambil data Nama
+          $nrp = $row['B']; // Ambil data nrp
+          $pkt = $row['C']; // Ambil data pangkat
+          $jabatan = $row['D']; // Ambil data jabatan
+          $tempat = $row['E']; // Ambil data tempat
+          $tgl_lahir = $row['F']; // Ambil data tanggal lahir
+          $agama = $row['G']; // Ambil data agama
+          $suku = $row['H']; // Ambil data suku
+          $tmt_jab = $row['I']; // Ambil data tmt jab
+          $id_bagian = $row['J']; // Ambil data bagian
+          $id_instansi = $this->uri->segment(3); // Ambil data id instansi
+
+          // Cek jika semua data tidak diisi
+          if ($nama == "" && $nrp == "" && $pkt == "" && $jabatan == "" && $tempat == "" && $tgl_lahir == "" && $agama == "" && $suku == "" && $tmt_jab == "" && $id_bagian == "")
+            continue; // Lewat data pada baris ini (masuk ke looping selanjutnya / baris selanjutnya)
+
+          // Cek $numrow apakah lebih dari 1
+          // Artinya karena baris pertama adalah nama-nama kolom
+          // Jadi dilewat saja, tidak usah diimport
+          if ($numrow > 1) {
+            // Validasi apakah semua data telah diisi
+            $nama_td = (!empty($nama)) ? "" : " style='background: #E07171;'";
+            $nrp_td = (!empty($nrp)) ? "" : " style='background: #E07171;'";
+            $pkt_td = (!empty($pkt)) ? "" : " style='background: #E07171;'";
+            $jabatan_td = (!empty($jabatan)) ? "" : " style='background: #E07171;'";
+            $tempat_td = (!empty($tempat)) ? "" : " style='background: #E07171;'";
+            $tgl_lahir_td = (!empty($tgl_lahir)) ? "" : " style='background: #E07171;'";
+            $agama_td = (!empty($agama)) ? "" : " style='background: #E07171;'";
+            $suku_td = (!empty($suku)) ? "" : " style='background: #E07171;'";
+            $tmt_jab_td = (!empty($tmt_jab)) ? "" : " style='background: #E07171;'";
+            $id_bagian_td = (!empty($id_bagian)) ? "" : " style='background: #E07171;'";
+            $id_instansi_td = $this->uri->segment(3);
+
+            // Jika salah satu data ada yang kosong
+            if ($nama == "" or $nrp == "" or $pkt == "" or $jabatan == "" or $tempat == "" or $tgl_lahir == "" or $agama == "" or $suku == "" or $tmt_jab == "" or $id_bagian == "") {
+              $kosong++; // Tambah 1 variabel $kosong
+            } ?>
+
+            <tr>
+              <td <?= filter_var($nama_td, FILTER_DEFAULT) ?>> <?= filter_var($nama, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($nrp_td, FILTER_DEFAULT) ?>> <?= filter_var($nrp, FILTER_DEFAULT) ?> </td>
+              <td <?= filter_var($pkt_td, FILTER_DEFAULT) ?>> <?= filter_var($pkt, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($jabatan_td, FILTER_DEFAULT) ?>> <?= filter_var($jabatan, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($tempat_td, FILTER_DEFAULT) ?>> <?= filter_var($tempat, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($tgl_lahir_td, FILTER_DEFAULT) ?>> <?= filter_var($tgl_lahir, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($agama_td, FILTER_DEFAULT) ?>> <?= filter_var($agama, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($suku_td, FILTER_DEFAULT) ?>> <?= filter_var($suku, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($tmt_jab_td, FILTER_DEFAULT) ?>> <?= filter_var($tmt_jab, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($id_bagian_td, FILTER_DEFAULT) ?>> <?= filter_var($id_bagian, FILTER_DEFAULT) ?></td>
+              <td <?= filter_var($id_instansi_td, FILTER_DEFAULT) ?>> <?= filter_var($instansi, FILTER_DEFAULT) ?> </td>
+            </tr>
+        <?php
+          }
+
+          $numrow++; // Tambah 1 setiap kali looping
+        } ?>
+
+      </table>
+      <?php
+      // Cek apakah variabel kosong lebih dari 0
+      // Jika lebih dari 0, berarti ada data yang masih kosong
+      if ($kosong > 0) {
+      ?>
+        <script>
+          $(document).ready(function() {
+            // Ubah isi dari tag span dengan id jumlah_kosong dengan isi dari variabel kosong
+            $("#jumlah_kosong").html("<?= filter_var($kosong, FILTER_DEFAULT); ?>");
+
+            $("#kosong").show(); // Munculkan alert validasi kosong
+          });
+        </script>
+      <?php
+      } else { // Jika semua data sudah diisi
+      ?>
+        <hr>
+
+        <!-- Buat sebuah tombol untuk mengimport data ke database -->
+
+        <button type='submit' name='import'>Kirim</button>;
+        <a href="<?= filter_var(base_url("sium/formPolsek/") . $instansi, FILTER_DEFAULT) ?> ">Batalkan</a>;
+
+      <?php
+      }
+      ?>
+    </form>
   <?php
-    } else { // Jika semua data sudah diisi
-      echo "<hr>";
-
-      // Buat sebuah tombol untuk mengimport data ke database
-      echo "<button type='submit' name='import'>Kirim</button>";
-      // echo "<a href='".base_url("sium/polsek")."'>Cancel</a>";
-      echo "<a href='" . base_url("sium/form_polsek/") . $instansi . "'>Batalkan</a>";
-
-      //masih salah
-
-    }
-
-    echo "</form>";
   }
   ?>
 </body>
