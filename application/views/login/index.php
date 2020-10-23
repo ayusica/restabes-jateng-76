4fa6d5b14a89dd8fbe79392b4cd94954d8540085
ayusica <72453272+ayusica@users.noreply.github.com>
2020-10-23 09:25:36 +0700

    Update index.php

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -3,22 +3,22 @@
 		<div class="card px-1 py-1 m-0">
 			<div class="card-header">
 				<div class="card-title text-center">
-					<img src="<?= base_url('assets/img/polda.png'); ?>" alt="Polrestabes" style="width: 75px;">
+					<img src="<?= filter_var(base_url('assets/img/polda.png'), FILTER_DEFAULT); ?>" alt="Polrestabes" style="width: 75px;">
 					<h3>POLRESTABES SEMARANG</h3>
 				</div>
 			</div>
 			<div class="card-body">
 				<marquee onmouseover="this.stop();" onmouseout="this.start();" behavior="scroll" direction="left" style="color: white;">Selamat Datang di Sistem Informasi Data Personel Polrestabes Semarang! Ganti Password Anda Setelah Login Untuk Pertama Kali!</marquee>
-				<form class="form-signin mt-3" action="<?= base_url(); ?>login/index" method="post">
-					<?php echo $this->session->flashdata('msg'); ?>
-					<?= form_error('nrp', '<small class="text text-warning pl-10">', '</small>'); ?>
+				<form class="form-signin mt-3" action="<?= filter_var(base_url(), FILTER_DEFAULT); ?>login/index" method="post">
+					<?= filter_var($this->session->flashdata('msg'), FILTER_DEFAULT); ?>
+					<?= filter_var(form_error('nrp', '<small class="text text-warning pl-10">', '</small>'), FILTER_DEFAULT); ?>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
 						<input type="" class="form-control" id="nrp" placeholder="nrp" name="nrp" minlength="8" maxlength="18" onkeypress="return wajibAngka(event)">
 					</div>
-					<?= form_error('password', '<small class="text text-warning" pl-10>', '</small>'); ?>
+					<?= filter_var(form_error('password', '<small class="text text-warning" pl-10>', '</small>'), FILTER_DEFAULT); ?>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
@@ -45,9 +45,9 @@
 
 <!-- akhir container -->
 <!-- jQuery-->
-<script src="<?= base_url(); ?>assets/js/jquery.js'?>"></script>
+<script src="<?= filter_var(base_url(), FILTER_DEFAULT); ?>assets/js/jquery.js'?>"></script>
 <!-- Bootsrap -->
-<!-- <script src="<?php echo base_url() . 'assets/js/bootstrap.min.js' ?>"></script> -->
+<!-- <script src="<?= filter_var(base_url(), FILTER_DEFAULT) . 'assets/js/bootstrap.min.js' ?>"></script> -->
 <script type="text/javascript">
 	function wajibAngka(evt) {
 		var charCode = (evt.which) ? evt.which : event.keyCode
